a065623c29316bd3a9acaf9c1aa55723fd349305
Julien Adam <julien.adam@arcodia.fr>
2024-04-18 10:43:30 +0200

    Added warning in message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Tours!");
+Console.WriteLine("Hello, Tours, this is not an exercise!");
 Console.ReadLine();
